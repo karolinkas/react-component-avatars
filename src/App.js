14e84deb867e931b2c9141e6d19385e510c3ce06
karolin siebert <karolin@intellisense.io>
2018-04-17 00:49:09 +0200

    display images in grid

--- src/App.js
+++ src/App.js
@@ -7,16 +7,41 @@ function importAll(r) {
 
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-
 class App extends Component {
+  
+  grid () { 
+    const grid = [];
+    const gridElements = this.imageList();
+    
+    for (let i = 0; i < gridElements.length; i += 4) {
+          const oneRow = [];
+          oneRow.push(gridElements.slice(i, i + 4).map(item => {
+        return <div style={{display: 'inline-block'}}>{item}</div>
+    }))
+    grid.push(oneRow.map(itm => {return <div>{itm}</div>}))
+    }
+    return grid;
+   }
+
+  imageList (){
+    return images.map((image, i) => {
+      return (
+        <div key={i} className="grid">
+          <div className="col"><img src={images[i]} alt="avatar"/></div>
+        </div>
+      );
+    });
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Choose your avatar</h1>
-          <img src={images[0]} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <div> 
+        <div><img src={images[0]}/></div>
+        <div className="popover">
+          {this.grid()}
+        </div>
+    </div>
     );
   }
 }
