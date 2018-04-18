3bc0e39ca8d9a4d02585e7620f07a1924d5366b1
karolin siebert <karolin@intellisense.io>
2018-04-18 10:00:25 +0200

    extract child components and add hover state

--- src/App.js
+++ src/App.js
@@ -7,44 +7,80 @@ function importAll(r) {
 
 const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
-class App extends Component {
+
+class Image extends Component {
+  constructor (props){
+    super(props);
+    this.state = {...props.image};
+    this.state.selected = false;
+  }
+  selectIcon (){
+    this.setState({selected: true}); 
+  }
   
+  render (){
+    return (<div key={this.state.src.toString()} onClick={() => this.selectIcon()} className="image-wrapper">
+      <img className={["avatar", (this.state.selected ? "active" : null)].join(" ")} src={this.state.src} alt="avatar"/>
+      <div className={"overlay-background"}></div>
+    </div>);
+  }
+}
+
+class AvatarPicker extends Component {
+  constructor (props){
+    super(props);
+    this.state = {imageData: props.imageData};
+  }
+
   grid () { 
     const grid = [];
-    const gridElements = this.imageList();
+    this.gridElements = this.imageList();
     
-    for (let i = 0; i < gridElements.length; i += 4) {
+    for (let i = 0; i < this.gridElements.length; i += 4) {
           const oneRow = [];
-          oneRow.push(gridElements.slice(i, i + 4).map(item => {
-        return <div className="gridElement" style={{display: 'inline-block'}}>{item}</div>
+          oneRow.push(this.gridElements.slice(i, i + 4).map(item => {
+        return <div key={this.gridElements[i].key} className="gridElement" style={{display: 'inline-block'}}>{item}</div>
     }))
-    grid.push(oneRow.map(itm => {return <div>{itm}</div>}))
+    grid.push(oneRow.map(item => {return <div key={item.toString()}>{item}</div>}))
     }
     return grid;
   }
-
-  swapIcon (event){
-    console.log(event);
+  getCurrentImage (selectedImage, e){
+    console.log("selectedImage");
+    console.log(selectedImage);
+    console.log(e);
   }
 
   imageList (){
-    return images.map((image, i) => {
-      return (
-        <div key={i.toString()} onClick={this.swapIcon} className="imageWrapper">
-          <img src={images[i]} alt="avatar"/>
-        </div>
+    console.log(this);
+    return this.state.imageData.map((image, i) => {
+      return (<Image image={image} onClick={() => this.getCurrentImage(image)}/>
       );
     });
   }
+  
+  render (){
+    return (
+      <div className="popover">
+      {this.grid()}
+    </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {imageData: images.map((image, i) => {
+      return {"src" : image, "label": `Avatar ${i + 1}`, "id": i, "key": `key${i + 1}`};
+    })};
+  }
 
   render() {
-
     return (
       <div> 
-        <div><img alt="current" src={images[0]}/></div>
-        <div className="popover">
-          {this.grid()}
-        </div>
+        <div className="currentAvatar"><img alt="current" src={this.state.imageData[0].src}/></div>
+        <AvatarPicker {...this.state}/>
       </div>
     );
   }
